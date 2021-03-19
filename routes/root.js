5b05b02cebd2f9feb354d28bd2ffbde0314a442c
Dani Edo <dani.nur.afandi@gmail.com>
2021-03-20 01:28:36 +0700

    fastify-swagger setup

--- routes/root.js
+++ routes/root.js
@@ -4,4 +4,24 @@ module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     return { root: true }
   })
+  fastify.route({
+    method: 'GET',
+    url: '/hello',
+    schema: {
+      tags: [ 'healthcheck' ],
+      description: 'healthcheck endpoint to etermine services is up and running',
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            status: { type: 'string' },
+            timestamp: { type: 'string', format: 'date-time' }
+          }
+        }
+      }
+    },
+    handler: async (request, reply) => {
+      return { status: 'ok', timestamp: new Date().toISOString() }
+    }
+  })
 }
