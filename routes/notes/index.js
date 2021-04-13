658596499b3d31cac301c8984f261a9a98605579
Dani Edo <dani.nur.afandi@gmail.com>
2021-04-13 23:52:03 +0700

    put and delete notes

--- routes/notes/index.js
+++ routes/notes/index.js
@@ -60,4 +60,63 @@ module.exports = async function (fastify, opts) {
       return newNote
     }
   })
+
+  fastify.route({
+    method: 'PUT',
+    url: '/:id',
+    schema: {
+      tags: [ 'notes' ],
+      description: 'Update a note',
+      body: {
+        type: 'object',
+        properties: {
+          title: { type: 'string' },
+          body: { type: 'string' }
+        }
+      },
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            id: { type: 'number', description: 'Unique identifier for ...' },
+            title: { type: 'string' },
+            body: { type: 'string', description: 'Main content of the ...' }
+          }
+        }
+      }
+    },
+    handler: async (request, reply) => {
+      const { id } = request.params;
+      const { title, body } = request.body;
+      const updateNote = await notesDAL.updateNote(id, title, body);
+      return updateNote
+    }
+  })
+
+  fastify.route({
+    method: 'DELETE',
+    url: '/:id',
+    schema: {
+      tags: [ 'notes' ],
+      description: 'Delete a note - WARNING - PERMANENT',
+      params: {
+        type: 'object',
+        required: ['id'],
+        properties: {
+          id: { type: 'number' },
+        }
+      },
+      response: {
+        204: {
+          type: 'string',
+          default: 'no content'
+        }
+      }
+    },
+    handler: async (request, reply) => {
+      const { id } = request.params;
+      const updateNote = await notesDAL.deleteNote(id);
+      reply.status(204);
+    }
+  })
 }
--- service/notesDAL.js
+++ service/notesDAL.js
@@ -1,18 +1,32 @@
 const notesDAL = (db) => {
   const createNote = async (title, body) => {
     const {
-      id
+      id,
     } = await db.one(
       "INSERT INTO notes (title, body) VALUES ($1, $2) RETURNING id",
       [title, body]
     );
     return { id, title, body };
   };
-  const getNotes = () => {
-    return db.manyOrNone('SELECT id, title, body FROM notes');
-  }
 
-  return { createNote, getNotes };
+  const getNotes = () => {
+    return db.manyOrNone("SELECT id, title, body FROM notes");
+  };
+
+  const updateNote = async (id, title, body) => {
+    await db.one(
+      "UPDATE notes SET title = $1, body = $2 where id = $3 RETURNING id",
+      [title, body, id]
+    );
+
+    return { id, title, body };
+  };
+
+  const deleteNote = (id) => {
+    return db.query("DELETE FROM notes WHERE id = $1", [id]);
+  };
+
+  return { createNote, getNotes, updateNote, deleteNote };
 };
 
 module.exports = notesDAL;
